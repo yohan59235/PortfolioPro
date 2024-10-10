1e66f233bc3e635ac0b92973ed16b22752e9101b
Yohan <150443604+yohan59235@users.noreply.github.com>
2024-10-10 19:24:58 +0200

    premier commit pour la création du portfolio

---
+++ Index.js
@@ -0,0 +1,13 @@
+// const swiper = new Swiper('.Swiper_Projects', {
+//     slidesPerView : 1,
+//     spaceBetween: 10,
+//     loop : true,
+//     navigation : {
+//         nextEl : '.Swiper_Button_Next',
+//         prevEl : '.Swpier_Button_Prev',
+//     },
+//     pagination : {
+//       el : '.Swiper_Pagination',
+//       clickable : true
+//     }
+// });
