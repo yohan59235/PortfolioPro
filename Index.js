1803cdb7aac1e4d5d44b1d74537055e9b710e6b2
Yohan <150443604+yohan59235@users.noreply.github.com>
2026-02-20 19:31:53 +0100

    testing new front for portfolio

--- Index.js
+++ Index.js
@@ -1,34 +1,34 @@
-const slides = document.querySelectorAll(".carrousel_inner > div");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
+// const slides = document.querySelectorAll(".carrousel_inner > div");
+// const prevButton = document.getElementById("prev");
+// const nextButton = document.getElementById("next");
 
-let currentSlide = 0;
+// let currentSlide = 0;
 
-function showSlide(index) {
-  slides.forEach((slide) => {
-    slide.style.display = "none";
-  });
+// function showSlide(index) {
+//   slides.forEach((slide) => {
+//     slide.style.display = "none";
+//   });
 
-  slides[index].style.display = "block";
-}
+//   slides[index].style.display = "block";
+// }
 
-function showPrevSlide() {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-  showSlide(currentSlide);
-}
+// function showPrevSlide() {
+//   currentSlide--;
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   }
+//   showSlide(currentSlide);
+// }
 
-function showNextSlide() {
-  currentSlide++;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  showSlide(currentSlide);
-}
+// function showNextSlide() {
+//   currentSlide++;
+//   if (currentSlide >= slides.length) {
+//     currentSlide = 0;
+//   }
+//   showSlide(currentSlide);
+// }
 
-prevButton.addEventListener("click", showPrevSlide);
-nextButton.addEventListener("click", showNextSlide);
+// prevButton.addEventListener("click", showPrevSlide);
+// nextButton.addEventListener("click", showNextSlide);
 
-showSlide(currentSlide);
+// showSlide(currentSlide);
